fcffe17a05207b2dd87e6c9049846fdbeae23823
Mathieu <mathieu.coutantfleury@gmail.com>
2020-02-27 20:53:49 +0100

    Add table to save data from classify picture

--- similardbcnx/dao/dao_images.py
+++ similardbcnx/dao/dao_images.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = 'Mathieu COUTANT FLEURY'
-from similardbcnx.models.model_image import Image, ExifData, LnkImageExifData
+from similardbcnx.models.model_image import Image, ExifData, LnkImageExifData, ClassiData, LnkImageClassi
 
 
 class DaoImages(object):
@@ -28,10 +28,22 @@ class DaoImages(object):
         self.session.flush()
         return exif.id
 
+    def create_classi(self, name, id_img):
+        classi = self.fetch_classidata_by_key(name)
+        if not classi:
+            classi = ClassiData(name)
+            self.session.add(classi)
+            self.session.flush()
+
+        classi.images.append(self.fetch_images_by_id(id_img))
+        self.session.flush()
+        return classi.id
+
     def fetch_images_by_id(self, id_img):
         """ Function to return Image from Db
         :param id_img:
-        :return:
+        :return: Image
+        :rtype: Image
         """
         return self.session.query(Image).get(id_img)
 
@@ -39,9 +51,27 @@ class DaoImages(object):
         """ Function to return Image from Db
         :param filename:
         :type filename: str
-        :return:
+        :return: Image
+        :rtype: Image
         """
-        return self.session.query(Image).filter(Image.filename == filename)
+        lst_img = self.session.query(Image).filter(Image.filename == filename).all()
+        if not lst_img:
+            return None
+        return lst_img[0]
+
+    def fetch_image_by_id_exif(self, id_exif):
+        """ Function to get all images associate with exif data
+        :param id_exif:
+        :return: Image
+        """
+        return self.session.query(Image).join(LnkImageExifData).join(ExifData).filter(ExifData.id == id_exif)
+
+    def fetch_image_by_id_classi(self, id_classi_data):
+        """ Function to get all images associate with classi data
+        :param id_classi_data:
+        :return: Image
+        """
+        return self.session.query(Image).join(LnkImageClassi).join(ClassiData).filter(ClassiData.id == id_classi_data)
 
     def fetch_lst_exifdata_by_id_filename(self, id_filename):
         """function to return list of exif data
@@ -61,6 +91,24 @@ class DaoImages(object):
         if not lst_exif:
             return None
         return lst_exif[0]
+
+    def fetch_lst_classidata_by_id_filename(self, id_filename):
+        """function to return list of classi data
+        :param id_filename
+        :type id_filename: int
+        :return:
+        """
+        return self.session.query(ClassiData).join(LnkImageClassi).filter(LnkImageClassi.id_image == id_filename)
+
+    def fetch_classidata_by_key(self, key):
+        """ function to return classi data
+        :param key:
+        :return:
+        """
+        lst_classi = self.session.query(ClassiData).filter(ClassiData.name == key).all()
+        if not lst_classi:
+            return None
+        return lst_classi[0]
 
     def remove_image(self, id_img):
         img = self.session.query(Image).get(id_img)
--- similardbcnx/models/model_image.py
+++ similardbcnx/models/model_image.py
@@ -13,6 +13,10 @@ LnkImageExifData = Table('lnk_image_exifdata', BaseImage.metadata,
                          Column('id_image', Integer, ForeignKey('images.id')),
                          Column('id_exifdata', Integer, ForeignKey('exif_data.id'))
                          )
+LnkImageClassi = Table('lnk_image_classi', BaseImage.metadata,
+                       Column('id_image', Integer, ForeignKey('images.id')),
+                       Column('id_classi_data', Integer, ForeignKey('classi_data.id'))
+                       )
 
 
 class Image(BaseImage):
@@ -32,6 +36,8 @@ class Image(BaseImage):
     datcre = Column('datcre', DateTime, nullable=False, doc="""Date time of insert""")
 
     exif_datas = relationship('ExifData', secondary=LnkImageExifData, back_populates='images',
+                              lazy='subquery')
+    classi_datas = relationship('ClassiData', secondary=LnkImageClassi, back_populates='images',
                                lazy='subquery')
 
     def __init__(self, filename, path, width="", height=""):
@@ -53,7 +59,7 @@ class ExifData(BaseImage):
                 doc="""Primary key of exif data""")
 
     name = Column('name', String(1000), nullable=False, doc="""name of data of exif""")
-    value = Column('value', String(1000), nullable=False, doc="""value of data of exif""")
+    value = Column('val', String(1000), nullable=False, doc="""value of data of exif""")
     datcre = Column('datcre', DateTime, nullable=False, doc="""Date time of insert""")
     images = relationship(Image, secondary=LnkImageExifData, back_populates='exif_datas',
                           lazy='subquery')
@@ -61,6 +67,26 @@ class ExifData(BaseImage):
     def __init__(self, name, value):
         self.name = name
         self.value = value
+        self.datcre = datetime.today()
+
+    def __repr__(self):
+        return u'<%s (%s): (%s)>' % (self.__class__.__name__, self.id, self.name)
+
+
+class ClassiData(BaseImage):
+    """ Table of exif data"""
+    __tablename__ = 'classi_data'
+
+    id = Column('id', Integer(), primary_key=True, autoincrement=True,
+                doc="""Primary key of exif data""")
+
+    name = Column('name', String(1000), nullable=False, doc="""name of data of exif""")
+    datcre = Column('datcre', DateTime, nullable=False, doc="""Date time of insert""")
+    images = relationship(Image, secondary=LnkImageClassi, back_populates='classi_datas',
+                          lazy='subquery')
+
+    def __init__(self, name):
+        self.name = name
         self.datcre = datetime.today()
 
     def __repr__(self):
